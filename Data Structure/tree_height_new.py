da70708c49507bea03c5f21518b0073354be1b23
Tracy Miao <117967335+shuhanmomo@users.noreply.github.com>
2023-01-25 22:39:14 -0500

    compute tree height

--- Data Structure/tree_height_new.py
+++ Data Structure/tree_height_new.py
@@ -1,5 +1,16 @@
 # python3
+# Task. You are given a description of a rooted tree. Your task is to compute and output its height. Recall
+# that the height of a (rooted) tree is the maximum depth of a node, or the maximum distance from a
+# leaf to the root. You are given an arbitrary tree, not necessarily a binary tree.
 
+# Input Format. The first line contains the number of nodes 𝑛. The second line contains 𝑛 integer numbers
+# from −1 to 𝑛 − 1 — parents of nodes. If the 𝑖-th one of them (0 ≤ 𝑖 ≤ 𝑛 − 1) is −1, node 𝑖 is the root,
+# otherwise it’s 0-based index of the parent of 𝑖-th node. It is guaranteed that there is exactly one root.
+# It is guaranteed that the input represents a tree.
+
+# Constraints. 1 ≤ 𝑛 ≤ 105.
+
+# Output Format. Output the height of the tree.
 import sys, threading
 sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
